c34239f2e687b467dabedc16c6ead0f4135dc397
Nathan W Brei <nathan.w.brei@gmail.com>
2017-10-15 22:38:20 +0200

    Final round of changes before talk

--- src/barrier.c
+++ src/barrier.c
@@ -2,22 +2,27 @@
 #include<omp.h>
 
 void main(int argc, char** argv) {
-  #pragma omp parallel sections
+  #pragma omp parallel 
   {
-    #pragma omp section
-    printf("Thread 0 did some work.\n");
+    #pragma omp sections 
+    {
+      #pragma omp section
+      printf("Thread 0 did some work.\n");
 
-    #pragma omp section
-    printf("Thread 1 did some work.\n");
-  }
-  printf("Threads rejoin\n");
+      #pragma omp section
+      printf("Thread 1 did some work.\n");
+    } // Implicit barrier
+    #pragma omp sections nowait
+    {
+      #pragma omp section
+      printf("Thread 0 after barrier.\n");
 
-  #pragma omp parallel sections
-  {
-    #pragma omp section
-    printf("Thread 0 after barrier.\n");
+      #pragma omp section
+      printf("Thread 1 after barrier.\n");
+    } // No barrier
 
-    #pragma omp section
-    printf("Thread 1 after barrier.\n");
-  }
+    // All threads do some work 
+    #pragma omp barrier
+    // All threads do some work
+  } // Implicit barrier
 }
